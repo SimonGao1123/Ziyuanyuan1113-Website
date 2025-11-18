a7411f91a65f7bfccd1b730e8804c92e91912e44
Simon Gao <simongao398@gmail.com>
2025-11-18 13:39:52 -0500

    fixed backend

--- back-end/index.js
+++ back-end/index.js
@@ -13,17 +13,9 @@ const PORT = process.env.PORT || 3000;
 const staticPath = join(__dirname, "..", "front-end");
 app.use(express.static(staticPath));
 
-// Route for home page
-app.get("/", (req, res) => {
-  res.sendFile(join(staticPath, "index.html"));
-});
 
-// Example backend route
-app.get("/test", (req, res) => {
-  res.send("Backend server is working ✅");
-});
 
 // Start server (bind to 0.0.0.0 to accept external traffic inside container)
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}/`);
 });
